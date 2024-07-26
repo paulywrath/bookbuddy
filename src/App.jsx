aaaa775339b75547149109bc928107582248dd7d
@paulywrath <127055825+paulywrath@users.noreply.github.com>
2024-07-26 13:11:23 -0400

    Make route to book details component

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,18 @@ import { useState } from "react"
 import { Route, Routes } from "react-router-dom"
 import Home from "./components/Home"
 import Auth from "./components/Auth"
+import BookDetails from "./components/BookDetails";
 
 function App() {
 
-  const [token, setToken] = useState(`test`);
+  const [token, setToken] = useState(``);
 
   return (
     <>
       <Routes>
         <Route path ='/' element={<Home />} />
         <Route path ='/auth' element={<Auth setToken={setToken}/>} />
+        <Route path ='/books/:id' element={<BookDetails />} />
       </Routes>
     </>
   )
