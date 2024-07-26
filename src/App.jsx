d799ca5e95ed60244fd3767da03f4619b824a66a
@paulywrath <127055825+paulywrath@users.noreply.github.com>
2024-07-26 17:00:27 -0400

    Add navbar and path to account component

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,35 @@
 import { useState } from "react"
-import { Route, Routes } from "react-router-dom"
+import { Link, Route, Routes } from "react-router-dom"
 import Home from "./components/Home"
 import Auth from "./components/Auth"
-import BookDetails from "./components/BookDetails";
+import BookDetails from "./components/BookDetails"
+import Account from "./components/Account"
 
 function App() {
 
   const [token, setToken] = useState(``);
+  console.log(token);
 
   return (
     <>
+      <nav>
+        <h1>Your Bookbuddy</h1>
+        <section>
+          <Link to='/' >Book List</Link>
+          
+          {
+            token ? 
+              <Link to='/account' >My Account</Link>:
+              <Link to='/auth' >Log In</Link>
+          }
+        </section>
+      </nav>
+      
       <Routes>
         <Route path ='/' element={<Home />} />
         <Route path ='/auth' element={<Auth setToken={setToken}/>} />
         <Route path ='/books/:id' element={<BookDetails />} />
+        <Route path ='/account' element={<Account />} />
       </Routes>
     </>
   )
