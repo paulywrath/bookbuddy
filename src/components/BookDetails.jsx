cdb05128bc06e967b9d9177a2b31b83c57d31813
@paulywrath <127055825+paulywrath@users.noreply.github.com>
2024-07-27 14:56:08 -0400

    Send user to account page upon successful checkout

--- src/components/BookDetails.jsx
+++ src/components/BookDetails.jsx
@@ -28,10 +28,11 @@ export default function BookDetails({token}) {
     if (!token) {
       alert(`Log in to be able to checkout a book.`);
       navigate('/auth');
+      return
     } 
     
     try {
-      const response = await fetch(`https://fsa-book-buddy-b6e748d1380d.herokuapp.com/api/books/${id}`, {
+      await fetch(`https://fsa-book-buddy-b6e748d1380d.herokuapp.com/api/books/${id}`, {
         method: "PATCH",
         headers: {
           'Content-Type': 'application/json',
@@ -41,8 +42,7 @@ export default function BookDetails({token}) {
           available: false,
         })
       })
-      const result = await response.json();
-      console.log(result);
+      navigate('/account');
     } catch(e) {
       alert(e);
     }
